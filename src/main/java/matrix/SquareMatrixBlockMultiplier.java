fd19ac9978e5ffc9edfd53fd2da9d8365d088f85
Pavel Ivanenko <pivanenko@dataxu.com>
2015-04-20 10:28:19 +0300

    cleanup + j8

--- src/main/java/matrix/SquareMatrixBlockMultiplier.java
+++ src/main/java/matrix/SquareMatrixBlockMultiplier.java
@@ -2,6 +2,9 @@ package matrix;
 
 import org.apache.commons.lang3.Validate;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static matrix.ArraysUtil.copyBlockToMatrix;
 import static matrix.ArraysUtil.getSubMatrix;
 
@@ -13,27 +16,46 @@ public class SquareMatrixBlockMultiplier extends SerialMultiplier {
         this.blockSize = blockSize;
     }
     
-    //    @Override
     public Integer[][] multiply(Integer[][] a, Integer[][] b) {
         validate(a, b);
         validateSquare(a);
         validateSquare(b);
         validateDimension(a.length);
         validateDimension(b.length);
-        int blocksInDimensionNum = a.length / blockSize;
         
         Integer[][] result = new Integer[a.length][a.length];
+        
+        List<MatrixMultiplyTask> tasks = fillMultiplyTasks(a, b);
+        
+        tasks.forEach(this::multiply);
+        
+        gatherResults(result, tasks);
+        
+        return result;
+    }
+    
+    private void gatherResults(Integer[][] result, List<MatrixMultiplyTask> tasks) {
+        for (MatrixMultiplyTask task : tasks) {
+            copyBlockToMatrix(result, task.getHorisontalBlockNum() * blockSize, task.getVerticalBlockNum() * blockSize,
+                    task.getResult());
+        }
+    }
+    
+    private List<MatrixMultiplyTask> fillMultiplyTasks(Integer[][] a, Integer[][] b) {
+        int blocksInDimensionNum = a.length / blockSize;
+        
+        List<MatrixMultiplyTask> tasks = new ArrayList<>(blocksInDimensionNum * blocksInDimensionNum);
+        
         
         for (int i = 0; i < blocksInDimensionNum; i++) {
             for (int j = 0; j < blocksInDimensionNum; j++) {
                 Integer[][] horizontalStripe = getHorizontalStripe(a, j);
                 Integer[][] verticalStripe = getVerticalStripe(b, i);
                 MatrixMultiplyTask task = new MatrixMultiplyTask(horizontalStripe, verticalStripe, i, j);
-                super.multiply(task);
-                copyBlockToMatrix(result, i * blockSize, j * blockSize, task.getResult());
+                tasks.add(task);
             }
         }
-        return result;
+        return tasks;
     }
     
     private Integer[][] getHorizontalStripe(Integer[][] matrix, int verticalBlockIndex) {
